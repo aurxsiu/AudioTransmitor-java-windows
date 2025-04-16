eed8ebe5f34c15c38ff4971bac9de34ebb50a3ac
aurxsiu <xiushou71@qq.com>
2025-04-17 03:08:11 +0800

    end

--- src/main/java/com/aurxsiu/test/Main.java
+++ src/main/java/com/aurxsiu/test/Main.java
@@ -6,48 +6,35 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+/**
+ * ffmpeg -f dshow -i audio="CABLE Output (VB-Audio Virtual Cable)" -f s16le -acodec pcm_s16le -ar 44100 -ac 2 -*/
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        ServerSocket serverSocket = new ServerSocket(20232);
+        System.out.println("等待手机连接...");
+        Socket socket = serverSocket.accept();
+        System.out.println("客户端已连接：" + socket.getRemoteSocketAddress());
 
         ProcessBuilder pb = new ProcessBuilder(
-                "ffmpeg", "-f", "dshow",
-                "-i", "audio=VB-Audio Virtual Cable",
+                "ffmpeg",
+                "-f", "dshow",
+                "-i", "audio=CABLE Output (VB-Audio Virtual Cable)",
                 "-f", "s16le",
                 "-acodec", "pcm_s16le",
                 "-ar", "44100",
                 "-ac", "2", "-"
         );
         pb.redirectErrorStream(true);
+
         Process ffmpeg = pb.start();
-        InputStream audioInput = ffmpeg.getInputStream();
-        Socket client;
-        try (ServerSocket server = new ServerSocket(20232)) {
-            client = server.accept();
-            System.out.println("lianjie");
+        InputStream ffmpegOut = ffmpeg.getInputStream();
+        OutputStream socketOut = socket.getOutputStream();
 
-            OutputStream out = client.getOutputStream();
-            audioInput.transferTo(out);
+        byte[] buffer = new byte[8192];
+        int len;
+        while ((len = ffmpegOut.read(buffer)) != -1) {
+            socketOut.write(buffer, 0, len);
         }
-    }
-    public static void play(InputStream input) throws LineUnavailableException, IOException {
-        // 配置音频格式：16位、44100Hz、立体声、小端序
-        AudioFormat format = new AudioFormat(44100, 16, 2, true, false);
-
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-        SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(info);
-        speakers.open(format);
-        speakers.start();
-
-        byte[] buffer = new byte[4096];
-        int bytesRead;
-
-        while ((bytesRead = input.read(buffer)) != -1) {
-            speakers.write(buffer, 0, bytesRead);
-        }
-
-        speakers.drain();
-        speakers.close();
     }
 }
