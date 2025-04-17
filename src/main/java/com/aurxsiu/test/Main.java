51e6e992cfd69743b04bd8c007c7c47528219914
aurxsiu <xiushou71@qq.com>
2025-04-18 00:46:13 +0800

    完成声卡设备的设置功能

--- src/main/java/com/aurxsiu/test/Main.java
+++ src/main/java/com/aurxsiu/test/Main.java
@@ -1,28 +1,55 @@
 package com.aurxsiu.test;
 
-import javax.sound.sampled.*;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 /**
  * ffmpeg -f dshow -i audio="CABLE Output (VB-Audio Virtual Cable)" -f s16le -acodec pcm_s16le -ar 44100 -ac 2 -*/
 
 public class Main {
+    //todo 增加设备检测以及端口选择的功能,端口选择依赖设备检测
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(20233);
-        System.out.println("等待手机连接...");
-        Socket socket = serverSocket.accept();
-        System.out.println("客户端已连接：" + socket.getRemoteSocketAddress());
+        if(args.length!=1){
+            throw new RuntimeException("参数不合法:https://github.com/aurxsiu/AudioTransmitor-java-windows");
+        }else{
+            String s = args[0];
+            switch (s){
+                case "s"->{
+                    //todo 完善一下
+                    Preparation.setDevice();
+                }//set
+                case "d"->{
+                    defaultRun();
+                }//default
+            }
+        }
 
+    }
+
+    public static void defaultRun() throws Exception{
+        try (ServerSocket serverSocket = new ServerSocket(20233)) {
+            System.out.println("等待手机连接...");
+            Socket socket = serverSocket.accept();
+            System.out.println("客户端已连接：" + socket.getRemoteSocketAddress());
+            InputStream ffmpegOut = getInputStream();
+            OutputStream socketOut = socket.getOutputStream();
+
+            byte[] buffer = new byte[8192];
+            int len;
+            while ((len = ffmpegOut.read(buffer)) != -1) {
+                socketOut.write(buffer, 0, len);
+            }
+        }
+    }
+    private static InputStream getInputStream() throws Exception {
+        String device = Preparation.getDriveByDefault();
         ProcessBuilder pb = new ProcessBuilder(
                 "ffmpeg",
                 "-f", "dshow",
-                "-i", "audio=CABLE Output (VB-Audio Virtual Cable)",
+                "-i", "audio="+device,
                 "-f", "s16le",
                 "-acodec", "pcm_s16le",
                 "-ar", "44100",
@@ -32,14 +59,6 @@ public class Main {
         pb.redirectError(new File("ffmpeg_error.log"));  // 可以将错误输出到文件
 
         Process ffmpeg = pb.start();
-        InputStream ffmpegOut = ffmpeg.getInputStream();
-        OutputStream socketOut = socket.getOutputStream();
-
-        byte[] buffer = new byte[8192];
-        int len;
-        while ((len = ffmpegOut.read(buffer)) != -1) {
-//            System.out.println(new String(buffer));
-            socketOut.write(buffer, 0, len);
-        }
+        return ffmpeg.getInputStream();
     }
 }
---
+++ src/main/java/com/aurxsiu/test/Preparation.java
@@ -0,0 +1,55 @@
+package com.aurxsiu.test;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+
+public class Preparation {
+    public static final String defaultDeviceFileName = "defaultDevice";
+    public static String getDriveByDefault() throws  Exception{
+        URL resource = Preparation.class.getClassLoader().getResource("./"+defaultDeviceFileName);
+        if (resource != null) {
+            File file = new File(resource.getPath());
+            try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                String result = new String(fileInputStream.readAllBytes(),StandardCharsets.UTF_8);
+                if (!result.isEmpty()) {
+                    return result;
+                }
+            }
+        }
+        return setDevice();
+    }
+    public static String setDevice() throws Exception{
+        /*ffmpeg -list_devices true -f dshow -i dummy */
+        ProcessBuilder pb = new ProcessBuilder(
+                "ffmpeg",
+                "-list_devices","true",
+                "-f","dshow",
+                "-i","dummy"
+        );
+        pb.redirectErrorStream(true);  // 不将错误输出流合并到标准输出流
+        try {
+            Process ffmpeg = pb.start();
+            InputStream ffmpegOut = ffmpeg.getInputStream();
+            String s = new String(ffmpegOut.readAllBytes());
+            System.out.println(s);
+        }catch (Exception e) {
+            System.out.println("命令可能出错,可通过ffmpeg查询设备名称");
+        }
+        System.out.println("输入设备名:");
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+        File file = new File(Preparation.class.getClassLoader().getResource("").getPath()+"/"+defaultDeviceFileName);
+        file.createNewFile();
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write(result.getBytes(StandardCharsets.UTF_8));
+        }
+        return result;
+    }
+    public static void main(String[] args) throws Exception{
+        Preparation.getDriveByDefault();
+    }
+}
